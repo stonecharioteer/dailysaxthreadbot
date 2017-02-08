0d9dec9d0e10b2b5dd643573cb78f5857f64f09b
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2017-02-08 22:53:50 +0530

    Fixed shebang

--- delete_all.py
+++ delete_all.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import praw
 import time
 import datetime
@@ -7,6 +9,7 @@ if __name__ == "__main__":
     reddit = praw.Reddit('dailysaxthreadbot')
     u = reddit.redditor("dailysaxthreadbot")
     counter = 0
+    print("Preparing to delete my comments, boss.")
     for comment in u.comments.new(limit=1000):
         counter+=1
         comment.delete()
