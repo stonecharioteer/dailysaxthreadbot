add4582623dacb391d9fbeb9fd2f68113bb49e0f
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2017-02-08 22:48:01 +0530

    Move delete to new module.

---
+++ delete_all.py
@@ -0,0 +1,16 @@
+import praw
+import time
+import datetime
+
+if __name__ == "__main__":
+    print(datetime.datetime.now())
+    reddit = praw.Reddit('dailysaxthreadbot')
+    u = reddit.redditor("dailysaxthreadbot")
+    counter = 0
+    for comment in u.comments.new(limit=1000):
+        counter+=1
+        comment.delete()
+        print("Deleted {} comments.".format(counter))
+
+    print("Done")
+
